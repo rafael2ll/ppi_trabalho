ed0bfa30a86894b5ae859119f71d3836834c36f5
benitoSan <38819921+benitoSan@users.noreply.github.com>
2021-05-23 07:48:00 -0700

    Modificações
    Modificações

--- public/php/agendamento.php
+++ public/php/agendamento.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../../css/style.css">
     <title>Nova Consulta</title>
 
     <!-- Bootstrap CSS -->
@@ -15,7 +15,7 @@
 
 <body>
 <?php
-include "navbar.html";
+include "../navbar.html";
 ?>
 <main class="container p-3">
     <form action="php/agenda_consulta.php" method="POST">
@@ -77,8 +77,9 @@ include "navbar.html";
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-popRpmFF9JQgExhfw5tZT4I9/CI5e2QcuUZPOVXb1m7qUmeR2b50u+YFEYe1wgzy"
         crossorigin="anonymous"></script>
+<br>
 <?php
-include "../footer.html";
+include "../../footer.html";
 ?>
 </body>
 
